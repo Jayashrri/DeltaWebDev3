e2555bbf45cdddf3c1ef89fb368613295e1bdcc2
jayashrri <balaji.jayashrri@gmail.com>
2019-06-28 02:46:50 +0530

    Invalid Login Messages

--- loginuser.php
+++ loginuser.php
@@ -12,6 +12,7 @@ function loginuser() {
     $link=new mysqli("localhost","root","pass123","FormBuilder");
     $un=$_POST['username'];
     $pw=$_POST['psw'];
+    global $dispmessage;
     $sql="SELECT UN,PW FROM UserDets WHERE UN='$un' AND PW='$pw'";
     $check=$link->query($sql);
     if($check->num_rows==1){
@@ -19,7 +20,7 @@ function loginuser() {
         redirect();
     }
     else{
-        echo "Invalid Credentials";
+        $dispmessage="Invalid Credentials";
     }
     $link->commit();
     $link->close();
@@ -30,26 +31,28 @@ function signupuser() {
     $un=$_POST['username'];
     $pw=$_POST['psw'];
     $pwr=$_POST['psw-repeat'];
+    global $dispmessage;
     $sql="SELECT UN FROM UserDets WHERE UN='$un'";
     $check=$link->query($sql);
     if($check->num_rows==0){
         if($pw==$pwr){
             $sql="INSERT INTO UserDets VALUES ('$un','$pw')";
             $link->query($sql);
-            echo "Signup Successful";
+            $dispmessage="Signup Successful";
         }
         else{
-            echo "Passwords Do Not Match";
+            $dispmessage="Passwords Do Not Match";
         }
     }
     else{
-        echo "Username Already Exists";
+        $dispmessage="Username Already Exists";
     }
     $link->commit();
     $link->close();
 }
 
 session_start();
+$dispmessage="";
 switch($_POST['form']){
     case 'L':
         loginuser();
@@ -59,3 +62,21 @@ switch($_POST['form']){
         break;
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form Builder</title>
+        <link rel="stylesheet" href="homepage.css">
+    </head>
+    <body>
+        <h4 class="pagehead"><?php echo $dispmessage; ?></h4>
+        <script>
+            function backtohome(){
+                window.location="formbuilder.html";
+            }
+        </script>
+
+        <button class="firstbutton" type="button" onclick="backtohome();">Go Back To Home</button>
+    </body>
+</html>
