02226ff85b6f56ea9b1925e03c375f811a5aa7ad
jayashrri <balaji.jayashrri@gmail.com>
2019-07-02 21:39:11 +0530

    Login to View Responses

--- showresponse.php
+++ showresponse.php
@@ -15,25 +15,66 @@ $result=$link->query("SELECT * FROM FormList WHERE FormURL='$url'");
 while($row=mysqli_fetch_assoc($result)){
     $formname=$row['FormName'];
     $formdesc=$row['FormDesc'];
+    $permissions=$row['FormPermissions'];
+    $owner=$row['FormOwner'];
+}
+if($permissions=="Creator Only"){
+    $showresponse=0;
+}
+else{
+    $showresponse=1;
 }
 
 if(isset($_POST['submit'])){
-    header("Location: FormPages/".$url.".php");
-    die();
+    $sql="SELECT * FROM UserDets WHERE UN='$owner'";
+    $result=$link->query($sql);
+    $row=mysqli_fetch_assoc($result);
+    $givenun=$_POST['username'];
+    $givenpw=$_POST['psw'];
+    if($givenun==$row['UN'] && $givenpw==$row['PW']){
+        $showresponse=1;
+    }
 }
-
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <title><?php echo $formname; ?></title>
-        <link rel="stylesheet" href="formstyle.css">
+        <link rel="stylesheet" href="responsestyle.css">
+        <script src="responsedisplay.js" async></script>
     </head>
     <body>
-        <h1><?php echo $formname; ?></h1>
+        <h1 class="pagehead"><?php echo $formname; ?></h1>
         <h4 class="nexthead">Responses</h4>
-        <div class="formtable">
+        <div id="notallowed" <?php if ($showresponse==1){ echo 'style="display:none;"'; } ?>>
+            <b>You are not allowed to view the responses to this form unless you're the owner.</b><br>
+            <hr>
+            <button type="button" id="login" onclick="document.getElementById('loginform').style.display='block'">Login</button>
+        </div>
+
+        <div id="loginform" class="modal">
+            <span onclick="document.getElementById('loginform').style.display='none'"
+            class="close" title="Close">&times;</span>
+            <form class="modal-content" action="" method="POST">
+                <div class="container">
+                    <h1>Log In</h1>
+                    <hr>
+                    <label for="username"><b>Username</b></label>
+                    <input type="text" placeholder="Enter Username" name="username" required>
+        
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="psw" required>
+        
+                    <div class="container">
+                        <button type="button" onclick="document.getElementById('loginform').style.display='none'" class="cancelbtn">Cancel</button>
+                        <button type="submit" name="submit">Login</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <div class="formtable" <?php if ($showresponse==0){ echo 'style="display:none;"'; } ?>>
             <table>
                 <tr>
                 <?php
@@ -57,9 +98,7 @@ if(isset($_POST['submit'])){
                 ?>
             </table>
             <br>
-            <form method="post" action="">
-                <button type="submit" name="submit">Go Back To Form</button>
-            </form>
+            <button type="button" id="backtoform" onclick="window.location='formpage.php'">Go Back To Form</button>
         </div>
     </body>
 </html>
